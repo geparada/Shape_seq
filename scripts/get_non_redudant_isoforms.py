5047fe444200a366cb8b63e852e463b4d8911c80
geparada <geparada88@gmail.com>
2020-01-12 22:42:35 +0000

    Update get_non_redudant_isoforms.py

--- scripts/get_non_redudant_isoforms.py
+++ scripts/get_non_redudant_isoforms.py
@@ -3,9 +3,26 @@ import csv
 from Bio import SeqIO
 
 def main(fasta, knownCanonical):
-
-
-
+    
+    main_transcripts = set([])
+    
+    with open(knownCanonical) as knownCanonical_file:
+        reader = csv.DictReader(knownCanonical_file, delimiter="\t")
+        
+        for row in reader:
+            main_transcripts.append(row["transcript"])
+            
+    with open(fasta) as fasta_file:
+        
+        for t in  SeqIO.parse(fasta_file, "fasta"):
+            
+            ID =  t.id.split("|")[0]
+            
+            if ID in main_transcripts:
+                
+                print(">" + ID)
+                print(t.seq)
+            
 
 
 if __name__ == '__main__':
